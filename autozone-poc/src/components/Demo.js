381c7b4f865a6750aa187d5dc133fb06c0b49a39
Gokul K <gokulwebdeveloper@gmail.com>
2020-06-17 13:09:47 +0530

    webpack basic configuration

---
+++ autozone-poc/src/components/Demo.js
@@ -0,0 +1,14 @@
+import React, { Component,Fragment } from "react";
+import ReactDOM from "react-dom";
+import Welcome from './Welcome';
+
+const Demo = () => {
+  return (
+    <Fragment>
+      <Welcome name="Autozone" />
+    </Fragment>
+  );
+};
+
+
+export default Demo;
