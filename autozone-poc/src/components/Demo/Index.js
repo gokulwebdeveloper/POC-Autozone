183bce10da23c437afb4ae2c47e5891305a4776d
Gopal Rathi <gopal.p.rathi20@gmail.com>
2020-06-17 18:44:38 +0530

    added folder structure

--- autozone-poc/src/components/Demo/Index.js
+++ autozone-poc/src/components/Demo/Index.js
@@ -1,7 +1,8 @@
 import React, { Component,Fragment } from "react";
 import ReactDOM from "react-dom";
-import Welcome from './Welcome';
+import Welcome from '../Welcome';
 
+//Component structure 
 const Demo = () => {
   return (
     <Fragment>
