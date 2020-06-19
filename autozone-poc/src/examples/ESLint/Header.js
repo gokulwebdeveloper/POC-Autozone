284ef74642f559016ad9874f107b3124834a0a02
Siddhu Kaulapure <siddhu.akaulapure@gmail.com>
2020-06-19 11:55:38 +0530

    added example for ESLint

---
+++ autozone-poc/src/examples/ESLint/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Header = () => {
+    var retValue = "Welcome to ESLint";
+    return (
+        retValue
+    );
+};
+export default Header;
--- autozone-poc/src/index.js
+++ autozone-poc/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Demo from "./components/Demo/Demo";
+import TestESLintConfig from "./examples/ESLint/TestESLintConfig";
 
 const wrapper = document.getElementById("container");
-wrapper ? ReactDOM.render(<Demo />, wrapper) : false;
+//wrapper ? ReactDOM.render(<Demo />, wrapper) : false;
+
+// Added to test ESLint
+wrapper ? ReactDOM.render(<TestESLintConfig />, wrapper) : false;
