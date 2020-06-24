a62152d90a494e8284fb00a7a438864059da68fc
Gopal Rathi <gopal.p.rathi20@gmail.com>
2020-06-24 11:28:51 +0530

    Revert "creating home page"
    This reverts commit 5b9a40d4d9014f337967012c9b82af6c521b8e92.

--- autozone-poc/src/components/Welcome.js
+++ autozone-poc/src/components/Welcome.js
@@ -1,118 +1,17 @@
-import React from 'react';
-import BgImg from '../utlis/img/bg-img/bg-1.jpg';
-import BgImgTopCatArea1 from '../utlis/img/bg-img/bg-2.jpg';
-import BgImgTopCatArea2 from '../utlis/img/bg-img/bg-3.jpg';
-import BgImgTopCatArea3 from '../utlis/img/bg-img/bg-4.jpg';
-import BgImgCatArea from '../utlis/img/bg-img/bg-5.jpg';
-import PropTypes from 'prop-types';
-import Button from '../utlis/button/Button';
-import TopCatagoryArea from '../utlis/top_catagory_area/TopCatagoryArea';
-import SimpleSlider from '../utlis/slick_slider/SimpleSlider';
+import React, { Component,Fragment } from "react";
+import ReactDOM from "react-dom";
+import './Demo/Demo.css';
 
-const Welcome = props => {
-    const bgImgStyle = {
-        'backgroundImage': `url(${BgImg})`
-    }  
-    const bgImgTopCatArea1 = {
-      'backgroundImage': `url(${BgImgTopCatArea1})`
-  }  
-  const bgImgTopCatArea2 = {
-    'backgroundImage': `url(${BgImgTopCatArea2})`
-}  
-const bgImgTopCatArea3 = {
-  'backgroundImage': `url(${BgImgTopCatArea3})`
-}  
-const bgImgCatArea = {
-  'backgroundImage': `url(${BgImgCatArea})`
-}
 
-    return (
-      <div>
-        <section className="welcome_area bg-img" style={bgImgStyle}>
-          <div className="container h-100">
-            <div className="row h-100 align-items-center">
-              <div className="col-12">
-                <div className="hero-content">
-                  <h6>asoss</h6>
-                  <h2>New Collection</h2>
-                  <Button btnText="view collection" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <div className="top_catagory_area section-padding-80 clearfix">
-          <div className="container">
-            <div className="row justify-content-center">
-              <TopCatagoryArea topCatagoryAreaText="Clothing" style={bgImgTopCatArea1} />
-              <TopCatagoryArea topCatagoryAreaText="Shoes" style={bgImgTopCatArea2} />
-              <TopCatagoryArea topCatagoryAreaText="Accessories" style={bgImgTopCatArea3} />
-            </div>
-          </div>
-        </div>
-        <div className="cta-area">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <div className="cta-content background-overlay bg-img" style={bgImgCatArea}>
-                  <div className="h-100 d-flex align-items-center justify-content-end">
-                    <div className="cta--text">
-                      <h6>-60%</h6>
-                      <h2>Global Sale</h2>
-                      <Button btnText="Buy Now" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <section>
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <div className="section-heading text-center">
-                  <h2>Popular Products</h2>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <div className="popular-products-slides owl-carousel owl-theme owl-loaded">
-                  <SimpleSlider />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <div className="brands-area d-flex align-items-center justify-content-between">
-        <div class="single-brands-logo">
-            <img src='src/utlis/img/core-img/brand1.png' alt=""/>
-        </div>
-        <div class="single-brands-logo">
-            <img src='src/utlis/img/core-img/brand2.png' alt=""/>
-        </div>
-        <div class="single-brands-logo">
-            <img src='src/utlis/img/core-img/brand3.png' alt=""/>
-        </div>
-        <div class="single-brands-logo">
-            <img src='src/utlis/img/core-img/brand4.png' alt=""/>
-        </div>
-        <div class="single-brands-logo">
-            <img src='src/utlis/img/core-img/brand5.png' alt=""/>
-        </div>
-        <div class="single-brands-logo">
-            <img src='src/utlis/img/core-img/brand6.png' alt=""/>
-        </div>
-        </div>
-      </div>
-    );
+const Welcome = (props) => {
+  return (
+    <Fragment>
+      <h1>
+        Welcome To {props.name}
+      </h1>     
+    </Fragment>
+  );
 };
 
-Welcome.propTypes = {
-    
-};
 
 export default Welcome;
