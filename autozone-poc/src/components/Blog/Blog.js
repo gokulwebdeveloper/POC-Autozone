172844a44feee4c561d5f4d51867bdc7c2d8508d
Sowmiya89 <c.sowmiya89@gmail.com>
2020-07-14 10:57:05 +0530

    fixed css issue

--- autozone-poc/src/components/Blog/Blog.js
+++ autozone-poc/src/components/Blog/Blog.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "../Blog/Blog.css";
-import { BrowserRouter as Router,NavLink, Link } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink, Link } from "react-router-dom";
 import UseInfiniteScroll from "../../utlis/infiniteScroll/UseInfiniteScroll";
-import background from '../../utlis/img/azo-blog-img/background_image.jpg';
+import background from "../../utlis/img/azo-blog-img/background_image.jpg";
 import RetrieveJson from "../../utlis/RetrieveAPI/RetrieveJson";
 import ScrollUpArrowBtn from "../../utlis/button/ScrollUpArrowBtn";
 
 const Blog = (props) => {
-
   const [list, setList] = RetrieveJson("data/blog.json");
 
   const [listItems, setListItems] = useState(
@@ -38,42 +37,28 @@ const Blog = (props) => {
         <div key={item}>
           <div className="col-12 col-lg-6">
             <div className="single-blog-area mb-50">
-            <NavLink to = "/singleblog"
-                    to={{
-                      pathname: "/singleblog",
-                      propsFor: {
-                        img_id: regularBlogList[item].id
-                      },
-                    }}>
-              <img src={regularBlogList[item].image} alt="" />
-              <div className="post-title">
-                <a href="#">{regularBlogList[item].title}</a>
-              </div>
-              <div className="hover-content">
-                <div className="hover-post-title">
-                <NavLink
-                  to={{
-                    pathname: "/singleblog",
-                    propsFor: {
-                      imgpath: regularBlogList[item].image,
-                      title: regularBlogList[item].title,
-                      desc:regularBlogList[item].description,
-                     },
-                  }}
-                >{regularBlogList[item].title}</NavLink>
+              <NavLink
+                to="/singleblog"
+                to={{
+                  pathname: "/singleblog",
+                  propsFor: {
+                    img_id: regularBlogList[item].id,
+                  },
+                }}
+              >
+                <img src={regularBlogList[item].image} alt="" />
+                <div className="post-title">
+                  <a href="#">{regularBlogList[item].title}</a>
                 </div>
-                <p>{regularBlogList[item].content}</p>
-                  <NavLink to = "/singleblog"
-                    to={{
-                      pathname: "/singleblog",
-                      propsFor: {
-                        img_id: regularBlogList[item].id
-                      },
-                    }}
-                  >
+                <div className="hover-content">
+                  <div className="hover-post-title">
+                    <a href="#">{regularBlogList[item].title}</a>
+                  </div>
+                  <p>{regularBlogList[item].content}</p>
+                  <a href="#">
                     Continue reading <i className="fa fa-angle-right"></i>
-                  </NavLink >
-              </div>
+                  </a>
+                </div>
               </NavLink>
             </div>
           </div>
@@ -84,11 +69,11 @@ const Blog = (props) => {
 
   return (
     <div>
-       <ScrollUpArrowBtn />
+      <ScrollUpArrowBtn />
       <div
         data-testid="background"
         className="blog-background-image"
-        style={{backgroundImage:"url(" + background + ")" }}
+        style={{ backgroundImage: "url(" + background + ")" }}
       ></div>
       <div className="blog-wrapper section-padding-80">
         <div className="container">
