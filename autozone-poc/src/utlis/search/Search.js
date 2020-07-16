af1adac5e4738bdceb4db9d8cf4d4882fbfc3082
Subramani <manikandan.subramani@atos.net>
2020-07-16 23:18:40 +0530

    Resolved search reload issue and on click of shop menu display full product and slider issue fixed
    search issue and shop link display full product and slider issue fixed

--- autozone-poc/src/utlis/search/Search.js
+++ autozone-poc/src/utlis/search/Search.js
@@ -1,23 +1,40 @@
 import React, { Fragment } from "react";
+import { useHistory } from "react-router-dom";
+import { connect } from 'react-redux';
+import { searchProduct } from '../../Redux/actions';
 
-const Search = () => {
+const Search = (props) => {
+  
+  const history = useHistory();
+  function onSearchProduct(event){
+    if(event.key === 'Enter') {
+      let keyword = event.target.value;
+      props.searchProduct(props.productData, keyword);
+      history.push("/shops?search="+keyword);
+    }
+    
+  }
+
   return (
     <Fragment>
       <div className="search-area">
-        <form action="/shops" >
-          <input
+      <input
             type="search"
             name="search"
             id="headerSearch"
             placeholder="Type for search"
+            onKeyDown= {onSearchProduct}
           />
           <button>
             <i className="fa fa-search" aria-hidden="true"></i>
           </button>
-        </form>
       </div>
     </Fragment>
   );
 };
 
-export default Search;
+const mapStateToProps = ({ productData }) => ({
+  productData
+})
+
+export default connect(mapStateToProps, {searchProduct}) (Search);
