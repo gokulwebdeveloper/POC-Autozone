9f6d2123bfebefc9604163bc499e1b3a739d0836
Gopal Rathi <gopal.p.rathi20@gmail.com>
2020-07-23 14:43:23 +0530

    resolved Issue for Cart Page #59

--- autozone-poc/src/utlis/footer/Footer.js
+++ autozone-poc/src/utlis/footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
                 <div className="footer_menu">
                   <ul>
                     <li>
-                      <a href="shop.html">Shop</a>
+                    <NavLink  to="/shops">Shop</NavLink >
                     </li>
                     <li>
                     <NavLink  to="/blog">Blog</NavLink >
@@ -132,8 +132,8 @@ const Footer = () => {
               <p>
                 Copyright &copy;All rights reserved | This template is made with{" "}
                 <i className="fa fa-heart-o" aria-hidden="true"></i> by{" "}
-                <a href="https://colorlib.com" target="_blank">
-                  Colorlib
+                <a href="https://autozone.com" target="_blank">
+                  Autozone
                 </a>
               </p>
             </div>
