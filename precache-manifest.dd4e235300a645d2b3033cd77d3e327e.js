895aed35c7657c5dbf5644fbe0704fbed8f8668e
A737962 <aman.singh@atos.net>
2020-08-19 18:52:06 +0530

    Updates

---
+++ precache-manifest.dd4e235300a645d2b3033cd77d3e327e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1f9a57e5f00f6a5ba221c54e553c1a",
+    "url": "/POC-Autozone/index.html"
+  },
+  {
+    "revision": "d78b392c7c8d004bb04a",
+    "url": "/POC-Autozone/static/css/2.70967cc7.chunk.css"
+  },
+  {
+    "revision": "43e125d196d4818589b2",
+    "url": "/POC-Autozone/static/css/main.f558539b.chunk.css"
+  },
+  {
+    "revision": "d78b392c7c8d004bb04a",
+    "url": "/POC-Autozone/static/js/2.584fc149.chunk.js"
+  },
+  {
+    "revision": "fc86483b3f1ce74062e0459516d89b56",
+    "url": "/POC-Autozone/static/js/2.584fc149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e125d196d4818589b2",
+    "url": "/POC-Autozone/static/js/main.484f6f0c.chunk.js"
+  },
+  {
+    "revision": "44f5bc8774e830d31328",
+    "url": "/POC-Autozone/static/js/runtime-main.0ceb3fba.js"
+  },
+  {
+    "revision": "4dfb9d1976a2838d7d019e4ca2ff64d5",
+    "url": "/POC-Autozone/static/media/classy.4dfb9d19.eot"
+  },
+  {
+    "revision": "999ab95a97ed648512c4e9fd82441129",
+    "url": "/POC-Autozone/static/media/classy.999ab95a.woff"
+  },
+  {
+    "revision": "c6dd5e6e6b7590465f1175a6e8953565",
+    "url": "/POC-Autozone/static/media/classy.c6dd5e6e.ttf"
+  },
+  {
+    "revision": "f6fb6851d8bff61b70c88fd84fe85127",
+    "url": "/POC-Autozone/static/media/classy.f6fb6851.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/POC-Autozone/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/POC-Autozone/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/POC-Autozone/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/POC-Autozone/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/POC-Autozone/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1dc1dcc173d99bb6db4f4159db137a76",
+    "url": "/POC-Autozone/static/media/loginbg.1dc1dcc1.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/POC-Autozone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/POC-Autozone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/POC-Autozone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/POC-Autozone/static/media/slick.f97e3bbf.svg"
+  }
+]);
